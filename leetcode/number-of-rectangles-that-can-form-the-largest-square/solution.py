9010f294c18a3e9990b1a56e4c4de3ca538ee0bd
조정원 <77387798+ChoJardin@users.noreply.github.com>
2022-08-29 22:00:06 +0900

    updated at 2022. 8. 29. 오후 10:00:06

--- leetcode/number-of-rectangles-that-can-form-the-largest-square/solution.py
+++ leetcode/number-of-rectangles-that-can-form-the-largest-square/solution.py
@@ -4,10 +4,16 @@ class Solution:
     def countGoodRectangles(self, rectangles: List[List[int]]) -> int:
         # 둘 중 더 짦은 길이만 남겨서, 그 중 max 인 길이가 몇개가 있는지 센다!
         square_lengths = list(map(lambda x: min(x), rectangles))
-
-        length_cnt = dict()
-
-        for length in square_lengths:
-            length_cnt[length] = length_cnt.get(length, 0) + 1
-
-        return length_cnt[max(length_cnt.keys())]
+        
+        square_lengths.sort(reverse=True) 
+        
+        max_length = square_lengths[0] 
+        cnt = 0 
+        
+        for length in square_lengths: 
+            if length == max_length: 
+                cnt += 1 
+            else: 
+                break
+                
+        return cnt
